6533dc8f43552d35c75c039284236f5c414e3d12
Act42 <dimusya007@gmail.com>
2023-10-24 23:10:50 +0300

    Created hw5

---
+++ Client.py
@@ -0,0 +1,18 @@
+import socket
+
+def client():
+    host = "127.0.0.1"
+    port = 12345
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    for i in range(100):
+        message = f"Повідомлення номер {i + 1}".encode('utf-8')
+        message_length = len(message)
+        client_socket.send(message_length.to_bytes(4, byteorder='big'))
+        client_socket.send(message)
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    client()
---
+++ Server.py
@@ -0,0 +1,24 @@
+import socket
+
+def server():
+    host = "127.0.0.1"
+    port = 12345
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+
+    print("Очікування підключення...")
+    conn, addr = server_socket.accept()
+    print(f"З'єднано з {addr[0]}:{addr[1]}")
+
+    for i in range(100):
+        message_length_bytes = conn.recv(4)
+        message_length = int.from_bytes(message_length_bytes, byteorder='big')
+        message = conn.recv(message_length)
+        print(f"Сервер отримав: {message.decode('utf-8')}")
+
+    conn.close()
+
+if __name__ == "__main__":
+    server()
